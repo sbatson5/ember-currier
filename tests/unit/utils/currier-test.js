b229dd45e1cdc57de72fde9da698c894d52b3be6
Scott Batson <sjbats@email.com>
2017-05-11 14:44:10 -0400

    tests

--- tests/unit/utils/currier-test.js
+++ tests/unit/utils/currier-test.js
@@ -3,12 +3,32 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | currier');
 
-test('it works', function(assert) {
+test('it properly curries string concat', function(assert) {
+  assert.expect(1);
+
   let greeting = function(str1, str2) {
     return `${str1} ${str2}`;
   };
 
-  let greetingName = currier(greeting, 'Hello there, ');
+  let greetingName = currier(greeting, 'Hello there,');
 
   assert.equal(greetingName('Bob!'), 'Hello there, Bob!');
+});
+
+test('it can handle multiple calls', function(assert) {
+  assert.expect(2);
+
+  let sequence = function(beginning, end) {
+    let result = [];
+
+    for (let i = beginning; i <= end; i++) {
+      result.push(i);
+    }
+
+    return result;
+  };
+
+  let getDigits = currier(sequence, 1);
+  assert.deepEqual(getDigits(3), [1, 2, 3]);
+  assert.deepEqual(getDigits(5), [1, 2, 3, 4, 5]);
 });
